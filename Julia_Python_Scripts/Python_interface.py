0fb7f160cc5fcf2b0a0a2c27a0c07578360408ea
Juan David Benavides <jdbenavi@ncsu.edu>
2022-03-28 15:22:07 -0400

    python interface updates

--- Julia_Python_Scripts/Python_interface.py
+++ Julia_Python_Scripts/Python_interface.py
@@ -2,38 +2,83 @@
 ## Ongoing Process
 ## Juan Benavides 3/28/2022
 
+from cProfile import label
 import os
 import subprocess as sp
 import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-print("Launching NuXmv")
-nuxmv = sp.Popen('nuxmv -n 3 Cyber.smv ',stdout=sp.PIPE,universal_newlines=True)
+'''
+Specs to check:
+
+LTLSPEC G (time <= 170 -> tank <= 550); -- 0
+LTLSPEC G (time <= 170 -> tank <= 600); -- 1
+LTLSPEC G (time <= 170 -> tank <= 650); -- 2
+
+LTLSPEC G (time <= 170 -> tank >= 300); -- 3
+LTLSPEC G (time <= 170 -> tank >= 200); -- 4
+LTLSPEC G (time <= 170 -> tank >= 100); -- 5
+LTLSPEC G (time <= 170 -> tank >= 25); -- 6
+LTLSPEC G (time <= 170 -> tank >= 10); -- 7
+
+'''
+
+# get to the right directory
+os.chdir('..')
+os.chdir('NuXmv Models')
+
+# launch nuxmv and save counterexample trace down
+print("Launching NuXmv - This may take a minute")
+nuxmv = sp.Popen('nuxmv -n 3  CyberFalseFinite.smv ',stdout=sp.PIPE,universal_newlines=True)
 output = nuxmv.stdout.read()
 
-print("Tank States:")
-tank = re.findall("tank = [0-9]+",output)
+# get a list of states
+states = re.split("State:",output)
 
-for ind,state in enumerate(tank):
-    tank[ind] = int(re.findall("[0-9]+",state)[0])
-print(tank)
+# parse to get an array of tank and tank sensor states
+tanks = np.ones(len(states)) * 700
+sensors = np.ones(len(states)) * 700
+for time,state in enumerate(states):
+    if time == 0: continue
+    tank = re.findall("tank = [0-9]+",state)
+    sensor = re.findall("tankSensor = [0-9]+",state)
+    if len(tank) == 0:
+        tanks[time] = tanks[time-1]
+    else:
+        tank = re.findall("[0-9]+",tank[0])
+        tanks[time] = int(tank[0])
+    if len(sensor) == 0:
+        sensors[time] = sensors[time-1]
+    else:
+        sensor = re.findall("[0-9]+",sensor[0])
+        sensors[time] = int(sensor[0])
+tanks = tanks[1:-1]
+sensors = sensors[1:-1]
 
-if len(tank) != 0:
+
+#print(tanks)
+#print(sensors)
+   
+# Plot or print that no counterexample was found
+if len(tanks) != 0:
     # cut off at states we don't care about
-    if len(tank) > 170:
-        tank = tank[0:170]
-    time = np.arange(len(tank))
+    if len(tanks) > 170:
+        tanks = tanks[0:170]
+        sensors = sensors[0:170]
+    time = np.arange(len(tanks))
 
     fig, ax = plt.subplots()
-    ax.plot(time, tank)
+    ax.plot(time, tanks,label = "tank")
+    ax.plot(time,sensors,label='tank sensor')
 
     ax.set(xlabel='time (s)', ylabel='tank height (cm)',
         title='nuXmv Trace')
     ax.grid()
+    ax.legend()
 
     #fig.savefig("test.png")
     plt.show()
 else:
-    print("No Counterexample Found")
+    print("No Counterexample Found") 
 
